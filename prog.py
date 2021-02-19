ffb3ab0da2ceb4eb00614f1e7befe29bf7f594d4
toriningen <toriningen.me@gmail.com>
2021-02-19 18:52:55 +0200

    Приводим форматирование к более читабельному виду

--- prog.py
+++ prog.py
@@ -2,19 +2,24 @@ b = True
 
 while b == True:
    a = int(input())
+
    if a == 0:
        b = False
-       print ('dziekuje, koniec!')
+       print('dziekuje, koniec!')
        break
 
-   for i in range (a):
+   for i in range(a):
        odpowiedz = ""
-       if (i+1) % 2 == 0:
+
+       if (i + 1) % 2 == 0:
            odpowiedz += "parzysta"
        else:
            odpowiedz += "nieparzysta"
-       if (i+1) % 3 == 0:
+
+       if (i + 1) % 3 == 0:
            odpowiedz += ", wielokrotnosc 3"
+
        if (i + 1) % 5 == 0:
            odpowiedz += ", wielokrotnosc 5"
+
        print(f'{i + 1}, {odpowiedz}')
